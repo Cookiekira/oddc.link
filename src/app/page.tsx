6f32e6de2cc27bda0ae297004538d9225dffd54e
OddC <oddc2000@outlook.com>
2024-05-07 11:40:55 +1000

    chore: Update npm dependencies
    Update npm dependencies to the latest stable versions:
    - clsx: ^2.1.1
    - framer-motion: ^11.1.8
    - prettier: ^3.2.5
    - prettier-plugin-tailwindcss: ^0.5.14
    - tailwindcss: ^3.4.1
    - typescript: ^5
    
    This update ensures that the project is using the most recent versions of these dependencies, which may include bug fixes, performance improvements, and new features.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,33 @@
-export default function Home(){
+import { motion } from 'framer-motion';
+
+export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      Hello World
-    </main>
-  )
+    <div className="flex min-h-screen flex-col justify-center">
+      <main className="flex flex-col items-center px-4 py-16">
+        <div className="relative mb-6 mt-12 text-4xl font-bold md:mb-12 md:mt-24 md:text-6xl">
+          👋&nbsp; Hi, I&apos;m{' '}
+          <div className="group inline-flex">
+            <div
+              className="transition-all duration-500 
+              ease-in-out group-hover:-translate-y-44
+              group-hover:opacity-0
+            "
+            >
+              &lt;OddC/&gt;
+            </div>
+            <div
+              className="opacity-0 transition-all  duration-500 ease-in-out 
+              group-hover:-translate-x-[4.5em]
+              group-hover:opacity-100
+            "
+            >
+              Humphrey Wong
+            </div>
+          </div>
+        </div>
+        {/* <Typewriter snippets={descriptiveStatements} /> */}
+        {/* <Links links={links} /> */}
+      </main>
+    </div>
+  );
 }

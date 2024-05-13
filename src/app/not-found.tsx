ac797aef5c726695c2bd8fb9e3a83953bfcee7ac
OddC <oddc2000@outlook.com>
2024-05-13 13:29:11 +1000

    chore: Add NotFound component for handling 404 errors

---
+++ src/app/not-found.tsx
@@ -0,0 +1,31 @@
+'use client';
+import Link from 'next/link';
+
+export default function NotFound() {
+  return (
+    <div className="flex min-h-screen flex-col justify-center">
+      <main className="flex flex-col items-center">
+        <section className="relative mb-6 mt-6 font-bold ">
+          {/* Logo by @SAWARATSUKI */}
+          <img src="/404 NotFound.png" alt="404" className="w-96" />
+        </section>
+
+        <section className="flex gap-4">
+          <span
+            className="icon-[mdi--arrow-left] cursor-pointer text-3xl
+           
+            font-bold opacity-60 hover:opacity-100"
+            onClick={() => window.history.back()}
+          />
+
+          <Link href="/">
+            <span
+              className="icon-[mdi--home] cursor-pointer text-3xl
+            font-bold opacity-60 hover:opacity-100"
+            />
+          </Link>
+        </section>
+      </main>
+    </div>
+  );
+}

dd628988b9f853383b76a315aa540916ad4c583a
OddC <oddc2000@outlook.com>
2024-05-13 16:08:14 +1000

    fix: cloudflare function does not proxy blog correctly

--- functions/blog/[[catchall]].ts
+++ functions/blog/[[catchall]].ts
@@ -1,5 +1,9 @@
 export const onRequest: PagesFunction = async (context) => {
   const blogUrl = new URL('https://oddc.craft.me');
+  const path = context.params.catchall;
+  if (path) {
+    blogUrl.pathname = path instanceof Array ? path.join('/') : path;
+  }
   const request = new Request(blogUrl);
   return fetch(request);
 };
--- src/app/middleware.ts
+++
@@ -1,7 +0,0 @@
-import { type NextRequest, NextResponse } from 'next/server';
-
-export function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname.startsWith('/blog')) {
-    return NextResponse.rewrite('https://oddc.link/blog');
-  }
-}

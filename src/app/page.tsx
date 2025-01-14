c04307fb16699bb4459d4ea518af99aa9b85e2c3
OddC <oddc2000@outlook.com>
2025-01-14 16:28:44 +1100

    feat: Update  animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,30 @@
 import { Links } from '@/components/Links';
 import { introduction, links } from '@/data/me';
 import { Typewriter } from '@/components/Typewriter';
+import { SparklesText } from '@/components/ui/sparkles';
+import TextRotate from '@/components/ui/text-rotate';
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col justify-center">
       <main className="flex flex-col items-center px-4 py-16">
         <section className="relative mb-6 mt-6 text-4xl font-bold md:mb-12 md:mt-10 md:text-6xl">
-          👋&nbsp; Hi, I&apos;m{' '}
-          <div className="group inline-flex">
-            <div
-              className="transition-all duration-500 
-              ease-in-out group-hover:-translate-y-full
-              group-hover:opacity-0
-            "
-            >
-              &lt;OddC/&gt;
-            </div>
-            <div
-              className="absolute opacity-0 transition-all delay-100 duration-500 ease-in-out 
-              group-hover:translate-x-0
-              group-hover:opacity-100
-              md:translate-x-full
-            "
-            >
-              Hang
-            </div>
-          </div>
+          <div className="animate-wave inline-block origin-center whitespace-pre">👋 </div>
+          <SparklesText className="inline whitespace-pre font-normal" sparklesCount={2} text={` Hi, I'm `} />{' '}
+          <SparklesText
+            className="inline font-normal"
+            sparklesCount={3}
+            text={
+              <TextRotate
+                mainClassName="w-40 md:w-60"
+                texts={[`<OddC/>`, ` Hang`]}
+                staggerFrom={'first'}
+                staggerDuration={0.1}
+                transition={{ type: 'spring', damping: 30, stiffness: 200 }}
+                rotationInterval={5000}
+              />
+            }
+          />
         </section>
 
         <section className="mb-10 flex">

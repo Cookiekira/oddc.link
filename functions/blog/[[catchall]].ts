4c7cecbe81e1a97c06688933112b54c1bea66d5c
OddC <oddc2000@outlook.com>
2025-04-10 16:58:36 +1000

    Bring back functions

--- functions/blog/[[catchall]].ts
+++ functions/blog/[[catchall]].ts
@@ -1,16 +1,16 @@
 interface Env {
-  BlogURL: string;
+	BlogURL: string;
 }
 
 export const onRequest: PagesFunction<Env> = async (context) => {
-  if (!context.env.BlogURL) {
-    return new Response('BlogUrl is not defined', { status: 500 });
-  }
-  const blogUrl = new URL(context.env.BlogURL);
-  const path = context.params.catchall;
-  if (path) {
-    blogUrl.pathname = Array.isArray(path) ? path.join('/') : path;
-  }
-  const request = new Request(blogUrl);
-  return fetch(request);
+	if (!context.env.BlogURL) {
+		return new Response("BlogUrl is not defined", { status: 500 });
+	}
+	const blogUrl = new URL(context.env.BlogURL);
+	const path = context.params.catchall;
+	if (path) {
+		blogUrl.pathname = Array.isArray(path) ? path.join("/") : path;
+	}
+	const request = new Request(blogUrl);
+	return fetch(request);
 };
--- src/components/ui/text-rotate.tsx
+++ src/components/ui/text-rotate.tsx
@@ -74,20 +74,20 @@ const TextRotate = forwardRef<TextRotateRef, TextRotateProps>(
 		};
 
 		const elements = useMemo(() => {
-			const currentText = texts[currentTextIndex];
-			if (splitBy === "characters") {
-				const text = currentText.split(" ");
-				return text.map((word, i) => ({
-					characters: splitIntoCharacters(word),
-					needsSpace: i !== text.length - 1,
-				}));
-			}
-			return splitBy === "words"
-				? currentText.split(" ")
-				: splitBy === "lines"
-					? currentText.split("\n")
-					: currentText.split(splitBy);
-		}, [texts, currentTextIndex, splitBy]);
+      const currentText = texts[currentTextIndex]
+      if (splitBy === 'characters') {
+        const text = currentText.split(' ')
+        return text.map((word, i) => ({
+          characters: splitIntoCharacters(word),
+          needsSpace: i !== text.length - 1,
+        }))
+      }
+      return splitBy === 'words'
+        ? currentText.split(' ')
+        : splitBy === 'lines'
+        ? currentText.split('\n')
+        : currentText.split(splitBy)
+    }, [texts, currentTextIndex, splitBy])
 
 		const getStaggerDelay = useCallback(
 			(index: number, totalChars: number) => {

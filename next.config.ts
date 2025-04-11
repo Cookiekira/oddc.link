609c59c7af4a2a1ed47af6f043a915d144be112e
OddC <oddc2000@outlook.com>
2025-04-11 16:47:15 +1000

    Fix format

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,21 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  async rewrites() {
-    return {
-      beforeFiles: [
-        {
-          source: '/blog',
-          destination: process.env.BLOG_URL || '',
-        },
-        {
-          source: '/blog/:slug',
-          destination: process.env.BLOG_URL
-            ? `${process.env.BLOG_URL}/:slug`
-            : '',
-        },
-      ],
-    }
-  },
+	/* config options here */
+	async rewrites() {
+		return {
+			beforeFiles: [
+				{
+					source: '/blog',
+					destination: process.env.BLOG_URL || '',
+				},
+				{
+					source: '/blog/:slug',
+					destination: process.env.BLOG_URL ? `${process.env.BLOG_URL}/:slug` : '',
+				},
+			],
+		}
+	},
 }
 
 export default nextConfig

39c0a8181bb7b69fbc14b34ee4d8d018761738ab
OddC <oddc2000@outlook.com>
2025-04-11 16:46:27 +1000

    Try fix blog url rewriting

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,23 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-	/* config options here */
-	async rewrites() {
-		return [
-			{
-				source: '/blog',
-				destination: process.env.BLOG_URL || '',
-			},
-			{
-				source: '/blog/:slug',
-				destination: process.env.BLOG_URL ? `${process.env.BLOG_URL}/:slug` : '',
-			},
-		]
-	},
+  /* config options here */
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: '/blog',
+          destination: process.env.BLOG_URL || '',
+        },
+        {
+          source: '/blog/:slug',
+          destination: process.env.BLOG_URL
+            ? `${process.env.BLOG_URL}/:slug`
+            : '',
+        },
+      ],
+    }
+  },
 }
 
 export default nextConfig
